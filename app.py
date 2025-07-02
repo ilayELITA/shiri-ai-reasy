5809dc33825a9d6d0052e1fe1cb93b8ed4c993a8
ilay <ilay@ilays-MacBook-Pro.local>
2025-07-02 10:18:52 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+@app.route("/voice", methods=["POST"])
+def voice():
+    twiml = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say voice="female" language="he-IL">שלום, כאן שירי. איך אפשר לעזור?</Say>
+</Response>"""
+    return Response(twiml, mimetype="text/xml")
+
+if __name__ == "__main__":
+    app.run(port=5003)
